4cc45835c2264d9bf79f97697bbf97372496e7a1
Gaurav Polekar <gaurav.polekar@gmail.com>
2022-02-25 20:12:03 +0530

    Project initialization

---
+++ src/test/java/com/bba/seven/SevenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bba.seven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
